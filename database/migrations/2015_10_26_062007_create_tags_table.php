ee3f422e24631fc0d12f0a0542b3da32eec66dcd
Targaryen <gcy77@live.com>
2016-04-01 22:46:20 +0800

    关键词改为标签

--- database/migrations/2015_10_26_062007_create_tags_table.php
+++ database/migrations/2015_10_26_062007_create_tags_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateKeywordsTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateKeywordsTable extends Migration
      */
     public function up()
     {
-        Schema::create('keywords', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('value');
             $table->string('article_id');
