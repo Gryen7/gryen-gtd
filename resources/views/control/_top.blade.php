136e640046b52059d121759a326636db54a2b825
Targaryen <gcy77@live.com>
2016-04-04 00:30:12 +0800

    '控制面板头部'

--- resources/views/control/_top.blade.php
+++ resources/views/control/_top.blade.php
@@ -1,6 +1,6 @@
 <div class="tar-cp-top clearfix">
     <ul class="list-group pull-right clearfix">
-        <li class="list-group-item pull-left"><a href="{{url('/index')}}" target="_blank">去首页</a></li>
+        <li class="list-group-item pull-left"><a href="{{url('/')}}" target="_blank">去首页</a></li>
         <li class="list-group-item pull-left"><a href="" target="_blank">更新说明</a></li>
         <li class="list-group-item pull-left"><a href="" target="_blank">免责声明</a></li>
         <li class="list-group-item pull-left"><a href="" target="_blank">版本 0.0.1</a></li>
