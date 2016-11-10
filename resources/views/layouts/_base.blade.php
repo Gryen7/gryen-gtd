9dc1a74c2a0f23a4bea2c05b0f05fc383fafcdec
targaryen <gcy77@live.com>
2016-11-10 11:19:43 +0800

    js-load-back

--- resources/views/layouts/_base.blade.php
+++ resources/views/layouts/_base.blade.php
@@ -5,10 +5,10 @@
     <title>@section('title')Tar-Blog
         @show</title>
     <link rel="stylesheet" media="screen" charset="utf-8" href={{asset('css/app.css')}}>
-    <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 </head>
 <body>
 @section('base_content')
 @show
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
