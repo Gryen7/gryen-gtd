0f3a74c73905ffaf9a1e1e678a5bce1df13601e7
targaryen <gcy77@live.com>
2016-11-20 17:04:56 +0800

    show-todo-list-foreach

--- resources/views/control/todolist.blade.php
+++ resources/views/control/todolist.blade.php
@@ -41,26 +41,5 @@
             </div>
         </li>
         @endforeach
-        <li class="btn-toolbar col-xs-12">
-            <div class="btn-group">
-                <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">
-                    未开始
-                </button>
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">未开始</a></li>
-                    <li><a href="#">进行中</a></li>
-                    <li><a href="#">已完成</a></li>
-                </ul>
-            </div>
-            <div class="btn-group">
-                <span class="glyphicon glyphicon-arrow-right"></span>
-            </div>
-            <div class="btn-group">
-                <button class="btn btn-default">这是一条任务</button>
-                <button class="btn btn-default">2016.11.20</button>
-                <button class="btn btn-default">2016.11.21</button>
-                <button class="btn btn-default">delete</button>
-            </div>
-        </li>
     </ul>
 @endsection
