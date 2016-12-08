4c7bb97d038f44564bd8a48f210c12b72f5c8ed6
targaryen <gcy77@live.com>
2016-12-08 21:10:10 +0800

    nav-icon

--- resources/views/layouts/_control.blade.php
+++ resources/views/layouts/_control.blade.php
@@ -3,12 +3,12 @@
     @parent
     <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-2 tar-cp-nav">
+            <div class="col-xs-1 tar-cp-nav">
                 @include('control._nav')
             </div>
-            <div class="col-xs-2">
+            <div class="col-xs-1">
             </div>
-            <div class="col-xs-10">
+            <div class="col-xs-11">
                 @include('control._top')
                 <hr style="margin: 0.5rem auto">
                 <div class="navbar navbar-default tar-ctl-navbar">
