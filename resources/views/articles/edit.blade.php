272d9d3b40c3c3ef2421ad38a1bc74e716d15e70
targaryen <gcy77@live.com>
2016-12-18 02:37:45 +0800

    button-text

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -3,7 +3,7 @@
     <div class="tar-article-form">
     {!! Form::model($article , ['method' => 'POST' , 'action' => ['ArticlesController@update' , $article->id]]) !!}
     {!! Form::input('hidden','status',0) !!}
-    @include('articles._form' , ['submitButtonText' => 'Submit Article','submitButtonText2' => 'Save Article'])
+    @include('articles._form' , ['submitButtonText' => 'Submit','submitButtonText2' => 'Save'])
     {!! Form::close() !!}
     </div>
 @stop
