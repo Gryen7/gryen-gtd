1edaf37ab0ac09a07d2d47b058f3165975989c3c
targaryen <gcy77@live.com>
2016-12-18 02:36:04 +0800

    writer-adjust-ok

--- resources/views/articles/_form.blade.php
+++ resources/views/articles/_form.blade.php
@@ -1,6 +1,7 @@
-<div class="col-md-12">
+<div class="col-md-1"></div>
+<div class="col-md-10">
     <div class="form-group tar-artl-ssbtn">
-        <div class="btn-group pull-right">
+        <div class="btn-group-vertical">
             {!! Form::button($submitButtonText,['class' => 'btn btn-success','id' => 'submit-article']) !!}
             {!! Form::button($submitButtonText2,['class' => 'btn btn-primary','id' => 'save-article']) !!}
         </div>
@@ -9,6 +10,7 @@
         {!! Form::textarea('content',null,['class' => 'form-control','id'=>'content-textarea']) !!}
     </div>
     <div class="tar-artl-ttlbox">
-        {!! Form::text('title',null,['class' => 'form-control tar-artl-title','placeholder' =>'Please input title ...']) !!}
+        {!! Form::text('title',null,['class' => 'form-control tar-artl-title','placeholder' =>'Please input title ...', 'autocomplete' => 'off']) !!}
     </div>
 </div>
+<div class="col-md-1"></div>
