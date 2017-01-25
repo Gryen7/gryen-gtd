606768c55a079b02c6eb6fa5ec93df08ed583aa1
targaryen <gcy77@live.com>
2017-01-25 11:36:18 +0800

    导航栏颜色调整

--- resources/views/common/_nav.blade.php
+++ resources/views/common/_nav.blade.php
@@ -27,7 +27,7 @@
                 <li @if ($module === 'article')class="active" @endif><a href="{{url('/articles')}}">Articles</a></li>
                 <li @if ($module === 'about')class="active" @endif><a href="{{url('/about')}}">About</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                    <a href="#" class="dropdown-toggle tar-go" data-toggle="dropdown" role="button" aria-haspopup="true"
                        aria-expanded="false">GO!</a>
                     <ul class="dropdown-menu">
                         @if (Auth::check())
