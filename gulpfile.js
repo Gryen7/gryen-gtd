df3e4061f15b49c266080ef47367c13e16931faf
targaryen <gcy77@live.com>
2017-02-01 22:22:33 +0800

    phplint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 //noinspection NpmUsedModulesInstalled
 const elixir = require('laravel-elixir');
+const gulp = require('gulp');
+const phplint = require('phplint').lint;
+
+/**
+ * PHP 代码检查
+ * TODO 详细学习用法
+ */
+gulp.task('phplint', function (cb) {
+    phplint(['app/**/*.php'], {limit: 10}, function (err) {
+        if (err) {
+            cb(err);
+            process.exit(1);
+        }
+        cb();
+    });
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -16,12 +32,16 @@ elixir(function (mix) {
     //noinspection JSUnresolvedFunction
     mix.sass('lib.scss', 'public/dist/css/lib.css')
         .sass('app.scss', 'public/dist/css/app.css')
-        .sass('control.scss', 'public/dist/css/control.css')
-        .webpack(['home.js', 'article.js', 'control.js', 'about.js'],'./public/dist/js')
-        .copy([// 复制图片
+        .sass('control.scss', 'public/dist/css/control.css');
+
+    mix.webpack(['home.js', 'article.js', 'control.js', 'about.js'],'./public/dist/js');
+
+    mix.copy([// 复制图片
         'resources/assets/img'
     ], 'public/dist/img')
         .copy([// 复制 bootstrap 的字库
             'node_modules/bootstrap-sass/assets/fonts/bootstrap'
         ], 'public/dist/fonts/bootstrap');
+
+    mix.task('phplint', 'app/**/*.php');
 });
