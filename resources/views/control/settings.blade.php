3291f23a869317baf42ceec1e64bf34e4cff5294
targaryen <gcy77@live.com>
2017-02-01 23:45:55 +0800

    banner-setting

--- resources/views/control/settings.blade.php
+++ resources/views/control/settings.blade.php
@@ -2,8 +2,8 @@
 @section('subNavigation')
     <form class="navbar-form navbar-left">
         <div class="btn-group">
-            <a href="{{action('Control\SettingsController@site')}}" class="btn btn-default">Site Setting</a>
-            <a href="{{action('Control\SettingsController@banners')}}" class="btn btn-default">Banner Setting</a>
+            <a href="{{action('Control\SettingsController@site')}}" class="btn btn-default">站点设置</a>
+            <a href="{{action('Control\SettingsController@banners')}}" class="btn btn-default">首页推荐</a>
         </div>
     </form>
 @stop
