c9215f7a4bfbaa117ee28303f467683be1bfcdf9
targaryen <gcy77@live.com>
2017-02-14 16:36:28 +0800

    文件上传逻辑移到MODEL层

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2,68 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
+use App\Upload;
 use Illuminate\Support\Facades\Input;
 
 class FilesController extends Controller
 {
-    protected static $UPLOAD_PATH = '/uploads';
-
-    /* 允许上传的文件类型 */
-    protected static $ALLOW_FILE_TYPE = [
-        'image/jpeg',
-        'image/png',
-        'image/gif'
-    ];
-
-    /* 服务器端的文件路径 */
-    protected static $FILE_RETURN_PATH = [
-        'local' => 'filesystems.disks.local.root',
-        'public' => 'filesystems.disks.public.root',
-        's3' => 'filesystems.disks.s3.region' . '/' . 'filesystems.disks.s3.bucket',
-        'qiniu' => 'filesystems.disks.qiniu.domains.default',
-    ];
-
     /**
      * 上传图片
      * @return array
+     * @internal param null $File
+     * @internal param bool $fromServer
      * @internal param Request $request
      */
     public function upload()
     {
-        $Disk = \Storage::disk(env('DISK'));
         $File = Input::file('upload_file');
-
-        if (!$File->getMimeType() || !in_array($File->getMimeType(), self::$ALLOW_FILE_TYPE)) {
-            return $this->returnResults(null, 'error file type！', false);
-        }
-
-        $fileType = $File->getClientOriginalExtension();
-        $filePath = self::$UPLOAD_PATH . '/' . Carbon::now()->toDateString() . '/' . md5(Carbon::now()->timestamp) . '.' . $fileType;
-
-        $results = $Disk->put($filePath, file_get_contents($File));
-
-        if ($results) {
-            return $this->returnResults('//' . \Config::get(self::$FILE_RETURN_PATH[env('DISK')]) . $filePath);
-        } else {
-            return $this->returnResults(null, 'error!', false);
-        }
+        return Upload::upload($File);
     }
 
-    /**
-     * 处理返回信息，兼容 simditor
-     *
-     * @param string $file_path
-     * @param string $msg
-     * @param bool $success
-     * @return array
-     */
-    private function returnResults($file_path = '', $msg = 'success!', $success = true)
-    {
-        return [
-            'file_path' => $file_path,
-            'msg' => $msg,
-            'success' => $success,
-        ];
-    }
 }
--- app/Upload.php
+++ app/Upload.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Upload extends Model
@@ -12,4 +13,70 @@ class Upload extends Model
         'url',
         'status'
     ];
+
+    protected static $UPLOAD_PATH = '/uploads';
+
+    /* 允许上传的文件类型 */
+    protected static $ALLOW_FILE_TYPE = [
+        'image/jpeg',
+        'image/png',
+        'image/gif'
+    ];
+
+    /* 服务器端的文件路径 */
+    protected static $FILE_RETURN_PATH = [
+        'local' => 'filesystems.disks.local.root',
+        'public' => 'filesystems.disks.public.root',
+        's3' => 'filesystems.disks.s3.region' . '/' . 'filesystems.disks.s3.bucket',
+        'qiniu' => 'filesystems.disks.qiniu.domains.default',
+    ];
+
+    /**
+     * 处理返回信息，兼容 simditor
+     *
+     * @param string $file_path
+     * @param string $msg
+     * @param bool $success
+     * @return array
+     */
+    private static function returnResults($file_path = '', $msg = 'success!', $success = true)
+    {
+        return [
+            'file_path' => $file_path,
+            'msg' => $msg,
+            'success' => $success,
+        ];
+    }
+
+
+    /**
+     * 上传图片
+     * @param null $File
+     * @return array
+     * @internal param bool $fromServer
+     * @internal param Request $request
+     */
+    public static function upload($File = null)
+    {
+        $Disk = \Storage::disk(env('DISK'));
+
+        if (!$File) {
+            return self::returnResults(null, 'error!', false);
+        }
+
+        if (!$File->getMimeType() || !in_array($File->getMimeType(), self::$ALLOW_FILE_TYPE)) {
+            return self::returnResults(null, 'error file type！', false);
+        }
+
+        $fileType = $File->getClientOriginalExtension();
+        $filePath = self::$UPLOAD_PATH . '/' . Carbon::now()->toDateString() . '/' . md5(Carbon::now()->timestamp) . '.' . $fileType;
+
+        $results = $Disk->put($filePath, file_get_contents($File));
+
+        if ($results) {
+            return self::returnResults('//' . \Config::get(self::$FILE_RETURN_PATH[env('DISK')]) . $filePath);
+        } else {
+            return self::returnResults(null, 'error!', false);
+        }
+    }
 }
