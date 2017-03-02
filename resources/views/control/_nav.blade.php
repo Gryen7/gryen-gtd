f06a83b112886beef0a9cbdb6094846b921e751e
targaryen <gcy77@live.com>
2017-03-02 22:43:25 +0800

    控制面板侧边栏中文化

--- resources/views/control/_nav.blade.php
+++ resources/views/control/_nav.blade.php
@@ -6,37 +6,37 @@
     </li>
     <li class="list-group-item">
         <a href="{{url('/control')}}">
-            <span class="glyphicon glyphicon-dashboard" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="Dashboard"></span>
+            <span class="glyphicon glyphicon-dashboard" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="状态"></span>
         </a>
     </li>
     <li class="list-group-item">
         <a href="{{url('/control/todos')}}">
-            <span class="glyphicon glyphicon-list-alt" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="TodoList"></span>
+            <span class="glyphicon glyphicon-list-alt" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="任务"></span>
         </a>
     </li>
     <li class="list-group-item">
         <a href="{{ action('ControlPanelController@articles') }}">
-            <span class="glyphicon glyphicon-pencil" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="Articles"></span>
+            <span class="glyphicon glyphicon-pencil" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="文章"></span>
         </a>
     </li>
     <li class="list-group-item">
         <a href="{{url('/control/comments')}}">
-            <span class="glyphicon glyphicon-comment" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="Comments"></span>
+            <span class="glyphicon glyphicon-comment" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="评论"></span>
         </a>
     </li>
     <li class="list-group-item">
         <a href="{{url('/control/user')}}">
-            <span class="glyphicon glyphicon-user" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="User"></span>
+            <span class="glyphicon glyphicon-user" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="用户"></span>
         </a>
     </li>
     <li class="list-group-item">
         <a href="{{url('/control/settings')}}">
-            <span class="glyphicon glyphicon-cog" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="Settings"></span>
+            <span class="glyphicon glyphicon-cog" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="设置"></span>
         </a>
     </li>
     <li class="list-group-item">
         <a href="{{url('/control/ashcan')}}">
-            <span class="glyphicon glyphicon-trash" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="Trash"></span>
+            <span class="glyphicon glyphicon-trash" onMouseOver="$(this).tooltip('show')" data-toggle="tooltip" data-placement="right" title="回收站"></span>
         </a>
     </li>
 </ul>
