ec615a2b3235cc6f4278bbdbb879f9661e0a676c
targaryen <gcy77@live.com>
2017-03-09 13:48:14 +0800

    首页相关两个表

---
+++ database/migrations/2017_03_09_053458_create_storys_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStorysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title'); // 标题
+            $table->text('resource'); // 音频资源链接
+            $table->string('images'); // 图片资源链接
+            $table->text('content'); // 文字描述
+            $table->tinyInteger('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storys');
+    }
+}
