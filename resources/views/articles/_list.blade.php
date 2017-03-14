534f66f983a175030b88ef9be3f0890f53c5b594
targaryen <gcy77@live.com>
2017-03-14 16:58:58 +0800

    article-style

--- resources/views/articles/_list.blade.php
+++ resources/views/articles/_list.blade.php
@@ -2,15 +2,15 @@
     @foreach($articles as $article)
         <li class="list-group-item row tar-article-list">
             <div class="col-md-6 t-rtcl-lf">
-                <h3>
+                <h4>
                     <a href="{{ action('ArticlesController@show',[$article->id]) }}">
                         <b>{{ $article->title }}</b>
                     </a>
-                </h3>
+                </h4>
                 <hr>
-                <span class="tar-article-extra">
-                    {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $article->updated_at)->toDateString() }}
-                </span>
+                <div class="tar-article-extra clearfix">
+                     <span class="pull-right">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $article->updated_at)->toDateString() }}</span>
+                </div>
                 <div class="tar-article-list-decs">{{ $article->description }}</div>
                 <div class="t-rtcl-tag">
                     @foreach($article->tags as $tag)
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts._default', ['module' => 'article'])
 @section('content')
     <div class="row">
-        <div class="col-md-1"></div>
-        <div class="col-md-10">
+        <div class="col-md-12">
             <div class="tar-article-box t-border-image">
                 <div class="text-center t-rtcl-ttl">{{ $article->title }}</div>
                 <hr>
-                <span class="pull-right tar-article-time">
-                {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $article->updated_at)->toDateString() }}
-            </span>
+                <div class="text-center tar-article-time clearfix">
+                   <span> {{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $article->updated_at)->toDateString() }}</span>
+            </div>
                 <div class="article-content">
                     {!! $article->content !!}
                 </div>
@@ -29,7 +28,6 @@
                 <a href="{{ action('ArticlesController@index') }}" class="btn btn-default">返回列表</a>
             </div>
         </div>
-        <div class="col-md-1"></div>
     </div>
 
     @if (Auth::check())
