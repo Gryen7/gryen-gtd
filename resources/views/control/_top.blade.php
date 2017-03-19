73d01f5fc781ea723d9277edafd772cb676202b1
targaryen <gcy77@live.com>
2017-03-19 20:56:24 +0800

    version-top-show

--- resources/views/control/_top.blade.php
+++ resources/views/control/_top.blade.php
@@ -3,6 +3,6 @@
         <li class="list-group-item pull-left"><a href="{{url('/')}}" target="_blank">站点首页</a></li>
         <li class="list-group-item pull-left"><a href="" target="_blank">更新日志</a></li>
         <li class="list-group-item pull-left"><a href="" target="_blank">免责声明</a></li>
-        <li class="list-group-item pull-left"><a href="" target="_blank">版本 {{ env('APP_VERSION') }}</a></li>
+        <li class="list-group-item pull-left"><a href="" target="_blank">版本 {{ config('app.version') }}</a></li>
     </ul>
 </div>
