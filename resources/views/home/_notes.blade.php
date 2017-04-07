b46088dc4e6eb3eba81a135afc8fe32012b397a1
targaryen <gcy77@live.com>
2017-04-08 01:53:54 +0800

    home-index-modified

--- resources/views/home/_notes.blade.php
+++ resources/views/home/_notes.blade.php
@@ -2,7 +2,7 @@
     <div class="text-center t-index-plttl t-border-image">志</div>
     <div class="row">
         @foreach($notes as $note)
-            <div class="col-xs-3 t-index-nt">
+            <div class="col-md-3 t-index-nt">
                 <a href="{{action('ArticlesController@show', ['id' => $note->id])}}" class="thumbnail">
                     <img class="lazy"
                          data-original="{{ imageView2($note->cover, ['w' => 400, 'h' => 300]) }}"
