1d6d9cfd2f95d0c741b759e966d1543a451769d0
Targaryen <gcy77@live.com>
2017-05-06 23:04:31 +0800

    min-height

--- resources/assets/js/common/index.js
+++ resources/assets/js/common/index.js
@@ -1,26 +1,9 @@
 /**
  * Created by targaryen on 16-9-4.
  */
-
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
-
-let tIndexFtr = $('.t-index-ftr');
 
 $.ajaxSetup({ headers: {
     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 } });
-
-/**
- * 页脚调整
- */
-$(function () {
-    let windowHeight = $(window).height();
-    let bodyHeight = $('body').height();
-
-    if (windowHeight > bodyHeight + 30) {
-        tIndexFtr.addClass('navbar-fixed-bottom').fadeIn();
-    } else {
-        tIndexFtr.fadeIn();
-    }
-});
--- resources/views/layouts/_default.blade.php
+++ resources/views/layouts/_default.blade.php
@@ -2,7 +2,7 @@
 @section('base_content')
     @parent
 @include('common._nav', ['extraClass' => ''])
-<div class="container-fluid">
+<div class="container-fluid t-page-minheight">
     <div class="row">
         @yield('content')
     </div>
