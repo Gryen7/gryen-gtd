3ab99da3a076bc5ecb44c7a74aa0c0d07bcebc5d
Targaryen <gcy77@live.com>
2017-05-09 16:35:29 +0800

    container

--- resources/views/layouts/_default.blade.php
+++ resources/views/layouts/_default.blade.php
@@ -2,7 +2,7 @@
 @section('base_content')
     @parent
 @include('common._nav', ['extraClass' => ''])
-<div class="container-fluid t-wrap">
+<div class="container t-wrap">
     <div class="row t-main">
         @yield('content')
     </div>
