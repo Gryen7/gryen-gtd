9e1ad5f6efa1e510b749beb2592a659356dc1be2
Targaryen <gcy77@live.com>
2017-05-19 01:34:11 +0800

    default-img

--- resources/views/articles/_full-screen-img.blade.php
+++ resources/views/articles/_full-screen-img.blade.php
@@ -1,3 +1,3 @@
 <div class="t-full-screen" id="tFullScreen">
-    <img id="tFullScreenImg" data-status="close" src="http://7xnswo.com1.z0.glb.clouddn.com/uploads/2017-05-18/106a413c582e9464bf9efc0e3b74c0f5.jpeg" alt="">
+    <img id="tFullScreenImg" data-status="close" src="" alt="">
 </div>
