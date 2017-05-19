fd2bfda5a23d32f90bee968bb1c3ab4d1ed7b658
Targaryen <gcy77@live.com>
2017-05-19 16:25:14 +0800

    nav-adjust

--- resources/views/common/_footer.blade.php
+++ resources/views/common/_footer.blade.php
@@ -1,7 +1,13 @@
 <footer class="navbar navbar-default t-index-ftr">
     <div class="container-fluid">
-        <p class="navbar-text">
-            <a href="mailto:targaryen@gryen.com‍" class="navbar-link">发电邮联系作者</a>
-        </p>
+        @if (Auth::check())
+            <p class="navbar-text">
+                <a href="{{ action('ControlPanelController@articles') }}" class="navbar-link">站点管理</a>
+            </p>
+        @else
+            <p class="navbar-text">
+                <a href="mailto:targaryen@gryen.com‍" class="navbar-link">发电邮联系作者</a>
+            </p>
+        @endif
     </div>
 </footer>
