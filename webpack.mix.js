296e2e6e4596b7b53a2315fb2336c8a485b2c4d1
Targaryen <gcy77@live.com>
2017-05-24 17:02:07 +0800

    sass-ok

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 const { mix } = require('laravel-mix');
 
+const PKG = require('./package.json');
+const DIST_PATH = 'public/dist';
+const DIST_PATH_WITH_VERSION = `${DIST_PATH}/${PKG.version}`;
+const AWESOME_FONT_PATH = 'node_modules/bootstrap-sass/assets/fonts/bootstrap/**.*';
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,6 +15,16 @@ const { mix } = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+/* bootstrap 字库 */
+mix.copy(AWESOME_FONT_PATH, `${DIST_PATH}/fonts`);
 
-mix.js('resources/assets/js/home/index.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
+/* 复制图片 */
+mix.copy('resources/assets/img', `${DIST_PATH}/img`);
+
+/* CSS 处理 */
+mix.sass('resources/assets/sass/lib.scss', `${DIST_PATH_WITH_VERSION}/css/lib.css`)
+    .sass('resources/assets/sass/app.scss', `${DIST_PATH_WITH_VERSION}/css/app.css`)
+    .sass('resources/assets/sass/control.scss', `${DIST_PATH_WITH_VERSION}/css/control.css`);
+
+/* JS 处理 */
+mix.js('resources/assets/js/home/index.js', `${DIST_PATH_WITH_VERSION}/js`);
