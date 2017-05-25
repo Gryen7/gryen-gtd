cd7b90f628b943671650843258ab7457d3249e3c
Targaryen <gcy77@live.com>
2017-05-25 13:50:25 +0800

    footer-style

--- resources/views/common/_footer.blade.php
+++ resources/views/common/_footer.blade.php
@@ -1,16 +1,13 @@
 <footer class="navbar navbar-default t-index-ftr">
     <div class="container-fluid">
-        @if (Auth::check())
             <p class="navbar-text">
-                <a href="{{ action('ControlPanelController@articles') }}" class="navbar-link">站点管理</a>
-            </p>
-        @else
-            <p class="navbar-text">
-                <a href="mailto:targaryen@gryen.com‍" class="navbar-link">发电邮联系作者</a>
-            </p>
-        @endif
-            <p class="navbar-text">
-                <a href="http://www.miitbeian.gov.cn/" class="navbar-link">鲁ICP备15009847号</a>
+                @if (Auth::check())
+                    <a href="{{ action('ControlPanelController@articles') }}" class="navbar-link">站点管理</a>
+                    <a href="{{ action('Auth\LoginController@logout') }}" class="navbar-link">登出</a>
+                @else
+                    <a href="mailto:targaryen@gryen.com‍" class="navbar-link">发电邮联系作者</a>
+                    <a href="http://www.miitbeian.gov.cn/" class="navbar-link">鲁ICP备15009847号</a>
+                @endif
             </p>
     </div>
 </footer>
