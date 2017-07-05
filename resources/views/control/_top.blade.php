e9c9a93dead211f614bfc02f6d01cc205a156f6a
Targaryen <gcy77@live.com>
2017-07-05 15:30:34 +0800

    up-version

--- resources/views/control/_top.blade.php
+++ resources/views/control/_top.blade.php
@@ -1,8 +1,8 @@
 <div class="tar-cp-top clearfix">
     <ul class="list-group pull-right clearfix">
         <li class="list-group-item pull-left"><a href="{{url('/')}}" target="_blank">站点首页</a></li>
-        <li class="list-group-item pull-left"><a href="https://github.com/itargaryen/laravelblog/blob/master/readme.md#版本更新说明" target="_blank">更新日志</a></li>
-        <li class="list-group-item pull-left"><a href="https://github.com/itargaryen/laravelblog/blob/master/readme.md#免责声明" target="_blank">免责声明</a></li>
+        <li class="list-group-item pull-left"><a href="https://gitee.com/targaryen/laravelblog/blob/master/readme.md#版本更新说明" target="_blank">更新日志</a></li>
+        <li class="list-group-item pull-left"><a href="https://gitee.com/targaryen/laravelblog/blob/master/readme.md#免责声明" target="_blank">免责声明</a></li>
         <li class="list-group-item pull-left">版本 {{ config('app.version') }}</li>
     </ul>
 </div>
