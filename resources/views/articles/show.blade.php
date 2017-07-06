8b45f3a3141fab83de5c20e7d283fc379cdb527e
Targaryen <gcy77@live.com>
2017-07-06 11:14:34 +0800

    163-tie

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,9 +5,12 @@
         <section class="article-content">
             {!! $article->content !!}
         </section>
-        <footer>
+        <footer class="clearfix">
             <span class="pull-right">本文更新于 <time pubdate >{{ $article->updated_at }}</time></span>
         </footer>
+        @if(env('APP_ENV') === 'production')
+        <div id="cloud-tie-wrapper" class="cloud-tie-wrapper"></div>
+        @endif
     </article>
     @if (Auth::check())
     <div class="form-group tar-artl-ssbtn">
@@ -17,4 +20,19 @@
     </div>
     @endif
     @include('articles._full-screen-img')
+    @if(env('APP_ENV') === 'production')
+    <!--suppress JSUnresolvedLibraryURL -->
+    <script src="https://img1.cache.netease.com/f2e/tie/yun/sdk/loader.js"></script>
+    <!--suppress ES6ConvertVarToLetConst, JSUnusedLocalSymbols -->
+    <script>
+        var cloudTieConfig = {
+            url: document.location.href,
+            sourceId: "",
+            productKey: "2fe43f2f72c64a53a5b027669a46038d",
+            target: "cloud-tie-wrapper"
+        };
+        var yunManualLoad = true;
+        Tie.loader("aHR0cHM6Ly9hcGkuZ2VudGllLjE2My5jb20vcGMvbGl2ZXNjcmlwdC5odG1s", true);
+    </script>
+    @endif
 @stop
