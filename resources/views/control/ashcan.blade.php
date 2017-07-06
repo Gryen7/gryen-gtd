11117d929a561c67aebcb83a374180d0f314359e
Targaryen <gcy77@live.com>
2017-07-06 16:33:06 +0800

    ashcan-style

--- resources/views/control/ashcan.blade.php
+++ resources/views/control/ashcan.blade.php
@@ -23,27 +23,4 @@
         @endforeach
     </ul>
     {{ $articles->links() }}
-    {{--模态框开始--}}
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                                aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Description</h4>
-                </div>
-                {!! Form::open() !!}
-                <div class="modal-body">
-                    <div class="form-group">
-                        {!! Form::input('text','description','',['class' => 'form-control','placeholder'=>'Please input description for the todo...']) !!}
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                    <button type="submit" class="btn btn-primary">确定</button>
-                </div>
-                {!! Form::close() !!}
-            </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
 @stop
