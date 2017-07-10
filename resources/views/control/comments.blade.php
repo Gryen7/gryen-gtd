a15b5c2825be6ed6ad72ce8e8407aba05e90588b
Targaryen <gcy77@live.com>
2017-07-10 11:28:28 +0800

    gentie-del

--- resources/views/control/comments.blade.php
+++ resources/views/control/comments.blade.php
@@ -1,4 +1,3 @@
 @extends('layouts._control', ['module' => 'control'])
 @section('content')
-    <iframe id="tCtlCmnt" src="https://manage.gentie.163.com/" frameborder="0" width="100%"></iframe>
 @stop
