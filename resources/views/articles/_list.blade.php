75165799ec6576ff6a594ba075a92baffbdd413b
Targaryen <gcy77@live.com>
2017-08-04 10:07:28 +0800

    article-list-style-modified

--- resources/views/articles/_list.blade.php
+++ resources/views/articles/_list.blade.php
@@ -1,15 +1,22 @@
 <ul class="col-md-10 col-md-offset-1">
     @foreach($articles as $article)
         <li class="list-group-item row tar-article-list">
-            <div class="col-md-12 t-artl-title">
-                <a href="{{ action('ArticlesController@show',[$article->id]) }}">
-                    {{ $article->title }}
-                </a>
-                <span class="t-artl-date pull-right">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s',
-                $article->updated_at)
-                ->toDateString() }}</span>
-            </div>
             <div class="col-md-6 t-rtcl-lf">
+                <div class="t-rtc-img">
+                    <a href="{{ action('ArticlesController@show',[$article->id]) }}">
+                        <img class="lazy"
+                             data-original="{{ imageView2($article->cover, ['w' => 600,'h' => 300]) }}"
+                             src="" alt="...">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 t-rtcl-rt">
+                <div class="t-artl-title">
+                    <a href="{{ action('ArticlesController@show',[$article->id]) }}">
+                        {{ $article->title }}
+                    </a>
+                    <span class="t-artl-date pull-right">{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $article->updated_at)->toDateString() }}</span>
+                </div>
                 <div class="tar-article-extra">
                     <div class="t-rtcl-tag">
                         @foreach($article->tags as $tag)
@@ -18,15 +25,6 @@
                     </div>
                 </div>
                 <div class="tar-article-list-decs">{{ $article->description }}</div>
-            </div>
-            <div class="col-md-6">
-                <div class="tar-article-info">
-                    <a href="{{ action('ArticlesController@show',[$article->id]) }}">
-                        <img class="lazy"
-                             data-original="{{ imageView2($article->cover, ['w' => 600,'h' => 300]) }}"
-                             src="" alt="...">
-                    </a>
-                </div>
             </div>
         </li>
     @endforeach
