52605f741c4c9f71434c51c5f5e31abc40b48e9a
Targaryen <gcy77@live.com>
2017-11-29 15:14:29 +0800

    ConfigManys-model-created

--- database/migrations/2017_11_03_172703_create_config_manys_table.php
+++ database/migrations/2017_11_03_172703_create_config_manys_table.php
@@ -15,13 +15,15 @@ class CreateConfigManysTable extends Migration
     {
         Schema::create('config_manys', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->integer('index');
-            $table->text('value');
+            $table->char('group', 8); // 配置组索引
+            $table->string('group_name'); // 配置组名称
+            $table->char('config', 8); // 配置索引
+            $table->string('config_name'); // 配置名称
+            $table->text('config_value'); // 配置值
             $table->tinyInteger('status');
-            $table->string('description');
+            $table->string('description'); // 额外的描述
             $table->timestamps();
-            $table->unique(['name', 'index']);
+            $table->unique(['group', 'config']);
         });
     }
 
