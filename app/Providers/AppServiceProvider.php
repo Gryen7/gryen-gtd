0f2af8286f6d8aafe1da0ff089e16298d5c194b2
Targaryen <gcy77@live.com>
2018-01-29 14:23:28 +0800

    boot-exception

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Config;
 use App\ConfigMany;
+use Exception;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('CONFIG', Config::getAllConfig());
-        view()->share('CONFIG_MANY', ConfigMany::getAllConfig());
+        try {
+            view()->share('CONFIG', Config::getAllConfig());
+            view()->share('CONFIG_MANY', ConfigMany::getAllConfig());
+        } catch (Exception $exception) {
+            logger('AppServiceProvider:boot share CONFIG CONFIG_MANY:Database is not initialized');
+        }
     }
 
     /**
