1d0a888e1205eda9b318318b6ec05b80c778e2d4
Targaryen <gcy77@live.com>
2018-01-29 13:40:43 +0800

    share

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->share('CONFIG', Config::getAllConfig());
-        view()->share('CONFIG_MANY', ConfigMany::getAllConfig());
+//        view()->share('CONFIG_MANY', ConfigMany::getAllConfig());
     }
 
     /**
