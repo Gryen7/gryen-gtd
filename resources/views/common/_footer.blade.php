06f62ba1ebe07e46421ba55978e09e733dcbadf4
Targaryen <gcy77@live.com>
2018-06-13 11:53:47 +0800

    bug-fixed

--- resources/views/common/_footer.blade.php
+++ resources/views/common/_footer.blade.php
@@ -5,7 +5,7 @@
                     <a href="{{ action('ControlPanelController@articles') }}" class="navbar-link">站点管理</a>
                     <a href="{{ action('Auth\LoginController@logout') }}" class="navbar-link">登出</a>
                 @else
-                    <a href="mailto:targaryen@gryen.com‍" class="navbar-link">发电邮联系作者</a>
+                    <a href="mailto:targaryen@gryen.com" class="navbar-link">发电邮联系作者</a>
                     <a href="http://www.miitbeian.gov.cn/" class="navbar-link">鲁ICP备15009847号</a>
                 @endif
             </p>
