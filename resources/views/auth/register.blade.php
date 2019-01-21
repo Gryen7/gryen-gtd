3794e47e61d309b29589e4288431d057019fe517
Targaryen <gcy77@live.com>
2019-01-21 15:16:56 +0800

    auth view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-md-8">
-        <div class="card">
+        <div class="card t-auth-card">
             <div class="card-header">{{ __('auth.Register') }}</div>
 
             <div class="card-body">
