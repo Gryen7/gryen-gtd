942d103fd11e9eb36d40e1fad0a8b1c4e95e13fa
Targaryen <gcy77@live.com>
2019-01-21 15:16:04 +0800

    auth view and trans

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts._default')
+
+@section('content')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">{{ __('auth.Verify Your Email Address') }}</div>
+
+            <div class="card-body">
+                @if (session('resent'))
+                    <div class="alert alert-success" role="alert">
+                        {{ __('auth.A fresh verification link has been sent to your email address.') }}
+                    </div>
+                @endif
+
+                {{ __('auth.Before proceeding, please check your email for a verification link.') }}
+                {{ __('auth.If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('auth.click here to request another') }}</a>.
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
