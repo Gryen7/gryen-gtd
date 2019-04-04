36fd8b1079c5aecbf8b1c31dea12fab7f16e770e
Gryen <gcy77@live.com>
2019-04-04 16:04:53 +0000

    Apply fixes from StyleCI

--- tests/Feature/Api/MetaWeblogTest.php
+++ tests/Feature/Api/MetaWeblogTest.php
@@ -6,48 +6,48 @@ use Tests\TestCase;
 
 class MetaWeblogTest extends TestCase
 {
-  public function testGetUsersBlogs()
-  {
-      $this->assertTrue(true);
-  }
+    public function testGetUsersBlogs()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testGetPost()
-  {
-      $this->assertTrue(true);
-  }
+    public function testGetPost()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testNewPost()
-  {
-      $this->assertTrue(true);
-  }
+    public function testNewPost()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testEditPost()
-  {
-      $this->assertTrue(true);
-  }
+    public function testEditPost()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testDeletePost()
-  {
-      $this->assertTrue(true);
-  }
+    public function testDeletePost()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testGetRecentPosts()
-  {
-      $this->assertTrue(true);
-  }
+    public function testGetRecentPosts()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testGetCategories()
-  {
-      $this->assertTrue(true);
-  }
+    public function testGetCategories()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testNewCategory()
-  {
-      $this->assertTrue(true);
-  }
+    public function testNewCategory()
+    {
+        $this->assertTrue(true);
+    }
 
-  public function testNewMediaObject()
-  {
-      $this->assertTrue(true);
-  }
+    public function testNewMediaObject()
+    {
+        $this->assertTrue(true);
+    }
 }
