2fb32b13149ec8d983079f2983485ce4daf1403f
Targaryen <gcy77@live.com>
2019-04-04 15:21:36 +0800

    modified codes and banner seeder done

---
+++ database/seeds/ArticleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Article::class, 5)
+            ->create()
+            ->each(function ($article) {
+                $article->withContent()->save(factory(\App\ArticleData::class)->make());
+                factory(\App\Banner::class)->create(['article_id' => $article->id]);
+                \App\Tag::createArticleTagProcess($article->tags, $article->id);
+            });
+    }
+}
