d9e1058d8ddcbea94391e513c50a86a9af6d8354
itargaryen <gcy77@live.com>
2020-10-28 10:22:14 +0800

    add views sum

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Article;
 use App\ConfigMany;
 use App\Http\Controllers\Controller;
-use Carbon\CarbonImmutable;
 
 class DashboardController extends Controller
 {
@@ -15,38 +14,9 @@ class DashboardController extends Controller
      */
     public function analytics()
     {
-        $articlesPublishedAts = Article::all(['published_at'])->filter(function ($article) {
-            return ! empty($article->published_at);
-        });
-        $count = $articlesPublishedAts->count();
+        $analytics = Article::analytics();
 
-        if ($count > 0) {
-            $maxDate = $articlesPublishedAts->max()->published_at;
-            $minDate = $articlesPublishedAts->min()->published_at;
-            $distance = $maxDate->diffInDays($minDate);
-            $aveRage = ceil($distance / $count);
-            $aveDistArr = $articlesPublishedAts->map(function ($value, $key) use ($articlesPublishedAts) {
-                if (isset($articlesPublishedAts[$key + 1])) {
-                    $curArtClCtdDt = $value->published_at;
-                    $nextArticleCtdDt = $articlesPublishedAts[$key + 1]->published_at;
-
-                    return $nextArticleCtdDt->diffInDays($curArtClCtdDt);
-                } else {
-                    return 0;
-                }
-            });
-
-            $aveDist = ceil(array_sum($aveDistArr->toArray()) / count($aveDistArr));
-        } else {
-            $maxDate = null;
-            $aveRage = null;
-            $aveDist = null;
-        }
-
-        $maxDateCal = empty($maxDate) ? null : $maxDate->calendar();
-        $disNow = empty($maxDate) ? null : CarbonImmutable::now()->diffInDays($maxDate);
-
-        return compact('aveRage', 'aveDist', 'maxDateCal', 'disNow');
+        return $analytics;
     }
 
     public function configManies()
--- resources/vue/components/dashboard/siteManager.vue
+++ resources/vue/components/dashboard/siteManager.vue
@@ -68,7 +68,7 @@
 export default {
   data: function() {
     return {
-      showAddConfig: true,
+      showAddConfig: false,
       configManies: []
     };
   },
