c0dc74e3e03754a7c0eb1b6fb9f63e989e990e55
Gryen <itargaryen@users.noreply.github.com>
2021-01-05 06:23:54 +0000

    Apply fixes from StyleCI

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -128,8 +128,7 @@ function trimAll($str)
     return str_replace($before, $after, $str);
 }
 
-
 function shouldInsertGTK(): bool
 {
-    return env('APP_ENV') === 'production' && !Auth::check() && !blank(env('GTM_CODE'));
+    return env('APP_ENV') === 'production' && ! Auth::check() && ! blank(env('GTM_CODE'));
 }
