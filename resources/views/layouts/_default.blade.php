d4b8bf76d5c6a50110a06835eca8438adaf0e951
Gryen <gcy77@live.com>
2022-03-22 11:19:31 +0800

    Update _default.blade.php

--- resources/views/layouts/_default.blade.php
+++ resources/views/layouts/_default.blade.php
@@ -35,39 +35,9 @@
                                 'csrfToken' => csrf_token(),
                             ]); ?>
     </script>
-    @if(shouldInsertGTK())
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', '<?php echo env('GTM_CODE'); ?>');
-    </script>
-    <!-- End Google Tag Manager -->
-    @endif
 </head>
 
 <body @if(isset($bodyClassString))class="{{ $bodyClassString }}" @endif>
-
-    @if(shouldInsertGTK())
-    <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="{{'https://www.googletagmanager.com/ns.html?id=' . env('GTM_CODE')}}" height="0" width="0" style="display:none;visibility:hidden">
-        </iframe>
-    </noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    @endif
-
     @include('common._nav', ['extraClass' => ''])
         <div class="container t-main" id="GryenApp">
             @yield('content')
